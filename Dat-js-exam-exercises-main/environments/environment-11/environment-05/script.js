87b7986a8c5e4497c91ba3ebc15cf39a6a525002
naja0001 <Naima_3108@hotmail.com>
2023-06-03 16:21:39 +0200

    ...

--- Dat-js-exam-exercises-main/environments/environment-11/environment-05/script.js
+++ Dat-js-exam-exercises-main/environments/environment-11/environment-05/script.js
@@ -1,6 +1,34 @@
 "use strict";
+/*
+
+console.log(courses);
+
+//1. Importer `courses`-listen i `script.js`.
+//2. Lav en funktion, der viser listen af alle `course`-objekter på websiden. Vis som minimum `name`, `ectsPoints` og `teacher`.
+//3. Lav en funktion der tilføjer et nyt `course`-objekt til listen. Sørg for at nye `course`-objekter vises på websiden.
+
+window.addEventListener("load", start);
+
+function start() {
+  showCourses();
+}
+
+function showCourses() {
+  for (const course of courses) {
+    const html =  `
+    
+    <li> ${course.name} - ${course.ectsPoints} - ${course.teacher} </li>
+    
+    `;
+    document
+      .querySelector("#courses-list")
+      .insertAdjacentHTML("beforeend", html);
+  }
+}
+*/
 
 import { courses } from "./courses.js";
+
 console.log(courses);
 
 //1. Importer `courses`-listen i `script.js`.
@@ -16,12 +44,20 @@ function start() {
 function showCourses() {
   for (const course of courses) {
     const html = /*html */ `
-    
     <li> ${course.name} - ${course.ectsPoints} - ${course.teacher} </li>
-    
     `;
     document
       .querySelector("#courses-list")
       .insertAdjacentHTML("beforeend", html);
   }
 }
+
+function createNewCourse(name, ectsPoints, teacher) {
+  const newCourse = { name, ectsPoints, teacher };
+
+  courses.push(newCourse);
+
+  showCourses();
+}
+
+createNewCourse("web development", 15, "Naima j");
--- Dat-js-exam-exercises-main/environments/environment-13/environment-05/script.js
+++ Dat-js-exam-exercises-main/environments/environment-13/environment-05/script.js
@@ -1,4 +1,5 @@
 "use strict";
+/*
 import { courses } from "./courses.js";
 console.log(courses);
 //1. Importér `courses`-listen i `script.js`.
@@ -18,7 +19,7 @@ function start() {
 //we dont need courses as arument because its alreade in our scope (global)
 function showCourses() {
   for (const course of courses) {
-    const html = /*html */ `
+    const html = `
     
     <li> ${course.name} - ${course.ectsPoints}</li>
     `;
@@ -52,12 +53,68 @@ function filterCourses() {
 
   //her filtrere vi hver enkelt course udfra
   for (const course of filteredCourses) {
-    const myhtml = /*html */ `
+    const myhtml =  `
     
     <li> ${course.name} - ${course.ectsPoints}</li>
     `;
     document
       .querySelector("#courses-list")
       .insertAdjacentHTML("beforeend", myhtml);
+  }
+}*/
+
+//1. Importér `courses`-listen i `script.js`.
+//2. Lav en funktion, der viser listen af alle `course`-objekter på websiden. Vis som minimum `name` og `ectsPoints`.
+//3. Lav en funktion, der filtrer listen af `courses` på baggrund af valgte `option` i `select` (se `environment-05`). Filtreringen ændrer sig hver gang en ny `option` vælges.
+
+import { courses } from "./courses.js";
+
+console.log(courses);
+
+window.addEventListener("load", start);
+
+function start() {
+  showCourses();
+
+  document.querySelector("#select-filter-ects").addEventListener("change", filteredCourses)
+}
+
+function showCourses() {
+  for (const course of courses) {
+    const html = /*html */ `
+    <li> ${course.name} - ${course.ectsPoints} </li>
+    `;
+    document
+      .querySelector("#courses-list")
+      .insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function filteredCourses() {
+  const selectValue = document.querySelector("#select-filter-ects").value;
+
+  let filteredCourse;
+
+  if (selectValue === "5") {
+    filteredCourse = courses.filter((course) => course.ectsPoints === 5);
+  } else if (selectValue === "10") {
+    filteredCourse = courses.filter((course) => course.ectsPoints === 10);
+  } else if (selectValue === "15") {
+    filteredCourse = courses.filter((course) => course.ectsPoints === 15);
+  } else if (selectValue === "20") {
+    filteredCourse = courses.filter((course) => course.ectsPoints === 20);
+  } else {
+    filteredCourse = courses;
+  }
+
+  document.querySelector("#courses-list").innerHTML = "";
+
+  for (const course of filteredCourse) {
+    const html = /*html */ `
+    <li> ${course.name} - ${course.ectsPoints} </li>
+    `;
+    document
+      .querySelector("#courses-list")
+      .insertAdjacentHTML("beforeend", html);
   }
 }
